74a7e1f726734b315c770607b352fe6aa2339235
vicknesh <vicknesh1997redblox@gmail.com>
2024-05-06 15:34:57 +0530

    User table created

---
+++ database/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'admin',
+  database: 'postgres',
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
